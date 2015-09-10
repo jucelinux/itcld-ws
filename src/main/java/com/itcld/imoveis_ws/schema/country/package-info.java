60ea26cb55fc7988b48d5ce694ddffa37176619f
Jucelino <jucelino.qjr@gmail.com>
2015-09-10 12:19:06 -0300

    * Atualização dos objetos gerados pelo xsd * Correção do arquivo de configuração;

--- src/main/java/com/itcld/imoveis_ws/schema/country/package-info.java
+++ src/main/java/com/itcld/imoveis_ws/schema/country/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2015.09.09 às 04:01:04 PM BRT 
+// Gerado em: 2015.09.09 às 04:35:18 PM BRT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://itcld.com/imoveis-ws/schema/country", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
